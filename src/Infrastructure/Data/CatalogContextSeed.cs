8ce1c67e11a1df7588f4b64306ef69f280e520f3
Bipeen Sinha <bipeen.sinha@hotmail.com>
2025-10-30 23:46:30 +0530

    Update CatalogContextSeed.cs

--- src/Infrastructure/Data/CatalogContextSeed.cs
+++ src/Infrastructure/Data/CatalogContextSeed.cs
@@ -1,54 +1,83 @@
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.eShopWeb.PublicApi;
-using Microsoft.eShopWeb.Infrastructure;
-using Microsoft.eShopWeb.Infrastructure.Data;
-using System.Net.Http;
 using System;
-using System.Linq;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.eShopWeb.ApplicationCore.Entities;
+using Microsoft.Extensions.Logging;
 
-namespace PublicApiIntegrationTests;
+namespace Microsoft.eShopWeb.Infrastructure.Data;
 
-[TestClass]
-public class ProgramTest
+public class CatalogContextSeed
 {
-    private static WebApplicationFactory<Program> _application = null!;
-
-    public static HttpClient NewClient => _application.CreateClient();
-
-    [AssemblyInitialize]
-    public static async Task AssemblyInitialize(TestContext _)
+    public static async Task SeedAsync(CatalogContext catalogContext, ILogger logger, int retry = 0)
     {
-        _application = new WebApplicationFactory<Program>()
-            .WithWebHostBuilder(builder =>
+        var retryForAvailability = retry;
+        try
+        {
+            if (catalogContext.Database.IsSqlServer())
             {
-                builder.ConfigureServices(services =>
-                {
-                    using var scope = services.BuildServiceProvider().CreateScope();
-                    var scopedServices = scope.ServiceProvider;
+                catalogContext.Database.Migrate();
+            }
 
-                    try
-                    {
-                        var db = scopedServices.GetRequiredService<CatalogContext>();
-                        var logger = scopedServices.GetRequiredService<ILogger<CatalogContextSeed>>();
+            if (!await catalogContext.CatalogBrands.AnyAsync())
+            {
+                await catalogContext.CatalogBrands.AddRangeAsync(GetPreconfiguredCatalogBrands());
+                await catalogContext.SaveChangesAsync();
+            }
 
-                        // Ensure database is created
-                        db.Database.EnsureCreated();
+            if (!await catalogContext.CatalogTypes.AnyAsync())
+            {
+                await catalogContext.CatalogTypes.AddRangeAsync(GetPreconfiguredCatalogTypes());
+                await catalogContext.SaveChangesAsync();
+            }
 
-                        // Run the seed logic defined in CatalogContextSeed.cs
-                        Task.Run(async () => await CatalogContextSeed.SeedAsync(db, logger)).GetAwaiter().GetResult();
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine($"[ERROR] Database initialization failed: {ex.Message}");
-                        throw;
-                    }
-                });
-            });
+            if (!await catalogContext.CatalogItems.AnyAsync())
+            {
+                await catalogContext.CatalogItems.AddRangeAsync(GetPreconfiguredItems());
+                await catalogContext.SaveChangesAsync();
+            }
+        }
+        catch (Exception ex)
+        {
+            if (retryForAvailability >= 10) throw;
 
-        await Task.CompletedTask;
+            retryForAvailability++;
+            logger.LogError(ex, "Error seeding database, retry count: {RetryCount}", retryForAvailability);
+
+            await SeedAsync(catalogContext, logger, retryForAvailability);
+        }
     }
+
+    private static IEnumerable<CatalogBrand> GetPreconfiguredCatalogBrands() => new List<CatalogBrand>
+    {
+        new("Azure"),
+        new(".NET"),
+        new("Visual Studio"),
+        new("SQL Server"),
+        new("Other")
+    };
+
+    private static IEnumerable<CatalogType> GetPreconfiguredCatalogTypes() => new List<CatalogType>
+    {
+        new("Mug"),
+        new("T-Shirt"),
+        new("Sheet"),
+        new("USB Memory Stick")
+    };
+
+    private static IEnumerable<CatalogItem> GetPreconfiguredItems() => new List<CatalogItem>
+    {
+        new(2, 2, ".NET Bot Black Sweatshirt", ".NET Bot Black Sweatshirt", 19.5M, "http://catalogbaseurltobereplaced/images/products/1.png"),
+        new(1, 2, ".NET Black & White Mug", ".NET Black & White Mug", 8.50M, "http://catalogbaseurltobereplaced/images/products/2.png"),
+        new(2, 5, "Prism White T-Shirt", "Prism White T-Shirt", 12, "http://catalogbaseurltobereplaced/images/products/3.png"),
+        new(2, 2, ".NET Foundation Sweatshirt", ".NET Foundation Sweatshirt", 12, "http://catalogbaseurltobereplaced/images/products/4.png"),
+        new(3, 5, "Roslyn Red Sheet", "Roslyn Red Sheet", 8.5M, "http://catalogbaseurltobereplaced/images/products/5.png"),
+        new(2, 2, ".NET Blue Sweatshirt", ".NET Blue Sweatshirt", 12, "http://catalogbaseurltobereplaced/images/products/6.png"),
+        new(2, 5, "Roslyn Red T-Shirt", "Roslyn Red T-Shirt", 12, "http://catalogbaseurltobereplaced/images/products/7.png"),
+        new(2, 5, "Kudu Purple Sweatshirt", "Kudu Purple Sweatshirt", 8.5M, "http://catalogbaseurltobereplaced/images/products/8.png"),
+        new(1, 5, "Cup<T> White Mug", "Cup<T> White Mug", 12, "http://catalogbaseurltobereplaced/images/products/9.png"),
+        new(3, 2, ".NET Foundation Sheet", ".NET Foundation Sheet", 12, "http://catalogbaseurltobereplaced/images/products/10.png"),
+        new(3, 2, "Cup<T> Sheet", "Cup<T> Sheet", 8.5M, "http://catalogbaseurltobereplaced/images/products/11.png"),
+        new(2, 5, "Prism White TShirt", "Prism White TShirt", 12, "http://catalogbaseurltobereplaced/images/products/12.png")
+    };
 }
